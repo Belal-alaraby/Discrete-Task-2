2decff09cc2b6efa7e7d57763c41751c63c8f2e4
Belal-alaraby <121315819+Belal-alaraby@users.noreply.github.com>
2022-12-23 23:57:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Project_2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ente the 1st number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("ente the 2nd number");
+            int y = int.Parse(Console.ReadLine());
+
+
+            for (int i = x; i <= y; i++)
+            {
+                int sum = 0;
+                for (int n = 1; n < i; n++)
+                {
+                    if (i % n == 0)
+                        sum = sum + n;
+                }
+                if (sum == i)
+
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
